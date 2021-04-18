1702f9f08998080c3975d0e24afe53eca39432fe
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-04-18 21:59:24 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -24,6 +24,7 @@ app.get("/", async (_, res) => {
 
 async function getStdinLine() {
     for await (const line of readLines(Deno.stdin)) {
+        console.log("GOT LINE: " + line)
         return line
     }
 }
